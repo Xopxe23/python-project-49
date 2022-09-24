5c118e535bf4150e5cd9c7c539d068a6254d15f0
George Dudaev <georgeduday@gmail.com>
2022-09-24 12:56:43 +0300

    fix prog

--- brain_games/prog.py
+++ brain_games/prog.py
@@ -5,15 +5,14 @@ import random
 def prog():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print('What number is missing in the progression?')
     count = 0
     while count < 3:
         number = random.randint(1, 20)
-        number_2 = random. randint(2, 5)
+        number_2 = random.randint(2, 5)
         progression = []
         for _ in range(number, number + 30, number_2):
             progression.append(_)
-        a = progression[random.randint(0, len(progression)-1)]
+        a = progression[random.randint(0, len(progression) - 1)]
         new_list = []
         for _ in progression:
             if _ == a:
@@ -27,6 +26,7 @@ def prog():
             print('Correct!')
         else:
             print(f"'{answer}' is wrong answer ;(. Correct answer was '{a}'.")
+            print(f"Let's try again, {name}!")
             break
     if count == 3:
         print(f'Congratulations, {name}!')
